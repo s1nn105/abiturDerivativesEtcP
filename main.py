d6a5a70d4455d620f365d60cb97f03dbb6f17e36
s1nn105 <s1nn105@web.de>
2020-04-16 14:14:34 +0200

    neue Datei:     main.py added basic main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from sympy import *
+from polynomial import *
+
+polynomial = create_polynomial()
+f= polynomial #everybody likes f(x) instead of polynomial(x)
+# first the derivative
+print(f"f(x)={f}")
