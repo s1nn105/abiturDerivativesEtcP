8dfc3bac0960a1a48cca0346b797bae699d4ee57
s1nn105 <s1nn105@web.de>
2020-04-16 14:36:47 +0200

    geändert:       main.py 	geändert:       polynomial.py added cli

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from sympy import *
 from polynomial import *
-
+import time
+init_printing()
 polynomial = create_polynomial()
+x = Symbol('x')
 f= polynomial #everybody likes f(x) instead of polynomial(x)
 # first the derivative
-print(f"f(x)={f}")
+print(pretty(f))
+print("Whats the derivative ? ")
+t1 = time.time()
+input("press any key if finished")
+t2=time.time()
+print(f"you needed {int(t2-t1)} seconds here is the derivative")
+td=int(t2-t1)
+
+print(pretty(simple(diff(f,x))))
+input("Ready ? ")
+print()
+print("Whats the Antiderivative ? ")
+print(pretty(Integral(f)))
+t1 = time.time()
+input("press any key if finished")
+t2=time.time()
+print(f"you needed {int(t2-t1)} seconds here is the derivative")
+
+print(pretty((integrate(f))))
+tad = int(t2-t1)
+print("The times :")
+print(f"f'(x):{td}")
+print(f"F(x):{tad}")
--- polynomial.py
+++ polynomial.py
@@ -4,7 +4,12 @@ MAX_DEGREE=5
 MIN_DEGREE=1
 MIN_FACT=1
 MAX_FACT=20
-
+DO_SIMPLIFY= False
+def simple(x):
+	if DO_SIMPLIFY:
+		return simplify(x)
+	else:
+		return x
 def create_polynomial(x=Symbol("x")):
 	degree =randint(MIN_DEGREE,MAX_DEGREE)
 	factors = [randint(MIN_FACT,MAX_FACT) for i in range(degree+1)]
