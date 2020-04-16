4b8002d5b8dd003bd6e6b8f242dba640e079dee6
s1nn105 <s1nn105@web.de>
2020-04-16 14:13:58 +0200

    neue Datei:     polynomial.py added create_polinomial function to create polnomials

---
+++ polynomial.py
@@ -0,0 +1,21 @@
+from sympy import *
+from random import randint
+MAX_DEGREE=5
+MIN_DEGREE=1
+MIN_FACT=1
+MAX_FACT=20
+
+def create_polynomial(x=Symbol("x")):
+	degree =randint(MIN_DEGREE,MAX_DEGREE)
+	factors = [randint(MIN_FACT,MAX_FACT) for i in range(degree+1)]
+	polynomial=[]
+	for i in range(degree,-1,-1):
+		polynomial.append(factors[i]*x**(degree-i))
+	#print(polynomial)
+	pol=polynomial.pop()
+	#print(pol)
+	for i in polynomial:
+		pol = pol+i
+	#print(pol)
+	return pol
+#create_polynomial()
